24595d3554d3038bed0eaa179dd874dd4d982b71
Tami Sweitzer <tamara0715@gmail.com>
2019-11-28 23:31:31 -0600

    Completed tutorial

---
+++ IntroASPNetMVCinCSharp/MVC_app/Controllers/PeopleController.cs
@@ -0,0 +1,28 @@
+﻿using MVC_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_app.Controllers
+{
+    public class PeopleController : Controller
+    {
+        // GET: People
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListPeople()
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            people.Add(new PersonModel { FirstName = "Tami", LastName = "Sweitzer", Age = 49 });
+            people.Add(new PersonModel { FirstName = "Toni", LastName = "Swesher", Age = 49 });
+            people.Add(new PersonModel { FirstName = "Tori", LastName = "Swetzler", Age = 49 });
+
+            return View(people);
+        }
+    }
+}
